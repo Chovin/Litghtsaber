756fe7e803d776d25df6df4c7669a8c8db5b1753
chovin <chovin@users.noreply.github.com>
2025-04-18 00:56:08 +1000

    make gpio global

--- game/controller.js
+++ game/controller.js
@@ -92,9 +92,8 @@ function newPeer() {
   
   getData((data, peerId) => {
     if (peerId == players[0]) {
-      pico8_gpio = data
+      window.pico8_gpio = data
     } 
-    console.log(data)
   })
 }
 try {
